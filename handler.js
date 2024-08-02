fb9732f044627d617eab14f13088321e298a36c9
JoaoEmboaba <jemboaba17@gmail.com>
2024-08-02 17:16:00 -0300

    feat: initial commit

---
+++ handler.js
@@ -0,0 +1,37 @@
+const uuid = require('uuid');
+const aws = require('aws-sdk');
+
+exports.generator = async (event) => {
+
+  const key = `${uuid.v4()}.png`;
+  const s3 = new aws.S3();
+
+  const params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key: key,
+    ContentType: 'application/png',
+    Expires: 30,
+  };
+
+  let presignedUrl;
+
+  try {
+    presignedUrl = await s3.getSignedUrlPromise('putObject', params);
+  } catch (error) {
+    console.error('Não foi possível gerar a URL pré assinada', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({error: 'Falha ao gerar a URL pré assinada'})
+    }
+  };
+
+  console.log(event);
+  console.log(presignedUrl);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      presignedUrl, key
+    })
+  };
+};
